0bba9eb96107116c1467d14554ca29cdff4e2e34
frankpagan <frank@cocreate.app>
2023-11-18 01:03:52 -0600

    feat: handle various data types returned from elements

--- src/index.js
+++ src/index.js
@@ -72,107 +72,126 @@ async function send(form) {
     let elements = form.querySelectorAll('[openai]')
 
     let conversation = forms.get(form)
-    if (!conversation) {
-        const crudReference = {
-            data: {
-                method: "database.create | database.read | database.update | database.delete | array.create | array.read | array.update | array.delete | index.create | index.read | index.update | index.delete | object.create | object.read | object.update | object.delete",
-                storage: "" || [""],
-                database: "" || [""],
-                array: "" || [""],
-                index: "" || [""],
-                object: {} || [{}],
-                filter: {
-                    query: [{ key: "", value: "", operator: "$eq | $ne | $includes", logicalOperator: "", caseSensitive: "true | false" }],
-                    sort: [{ key: "", direction: "asc | desc" }],
-                    search: [{ value: "", operator: "", caseSensitive: "true | false" }]
-                }
-            }
-        }
-        const crudObjectReference = {
-            data: {
-                method: " object.create | object.read | object.update | object.delete",
-                array: "" || [""],
-                object: {} || [{}],
-                filter: {
-                    query: [{ key: "", value: "", operator: "$eq | $ne | $includes", logicalOperator: "", caseSensitive: "true | false" }],
-                    sort: [{ key: "", direction: "asc | desc" }],
-                    search: [{ value: "", operator: "", caseSensitive: "true | false" }],
-                    limit: 0
-                }
-            }
-        }
+    // if (!conversation) {
+    //     const crudReference = {
+    //         data: {
+    //             method: "database.create | database.read | database.update | database.delete | array.create | array.read | array.update | array.delete | index.create | index.read | index.update | index.delete | object.create | object.read | object.update | object.delete",
+    //             storage: "" || [""],
+    //             database: "" || [""],
+    //             array: "" || [""],
+    //             index: "" || [""],
+    //             object: {} || [{}],
+    //             filter: {
+    //                 query: [{ key: "", value: "", operator: "$eq | $ne | $includes", logicalOperator: "", caseSensitive: "true | false" }],
+    //                 sort: [{ key: "", direction: "asc | desc" }],
+    //                 search: [{ value: "", operator: "", caseSensitive: "true | false" }]
+    //             }
+    //         }
+    //     }
+    //     const crudObjectReference = {
+    //         data: {
+    //             method: " object.create | object.read | object.update | object.delete",
+    //             array: "" || [""],
+    //             object: {} || [{}],
+    //             filter: {
+    //                 query: [{ key: "", value: "", operator: "$eq | $ne | $includes", logicalOperator: "", caseSensitive: "true | false" }],
+    //                 sort: [{ key: "", direction: "asc | desc" }],
+    //                 search: [{ value: "", operator: "", caseSensitive: "true | false" }],
+    //                 limit: 0
+    //             }
+    //         }
+    //     }
 
-        const filterObjectReference = {
-            data: {
-                method: " object.create | object.read | object.update | object.delete",
-                array: "files",
-                object: [{
-                    "_id": "",
-                    "name": "",
-                    "src": "",
-                    "host": [
-                        "*",
-                    ],
-                    "directory": "/",
-                    "path": "",
-                    "content-type": "",
-                    "public": "true"
-                }]
-            }
-        }
+    //     const filterObjectReference = {
+    //         data: {
+    //             method: " object.create | object.read | object.update | object.delete",
+    //             array: "files",
+    //             object: [{
+    //                 "_id": "",
+    //                 "name": "",
+    //                 "src": "",
+    //                 "host": [
+    //                     "*",
+    //                 ],
+    //                 "directory": "/",
+    //                 "path": "",
+    //                 "content-type": "",
+    //                 "public": "true"
+    //             }]
+    //         }
+    //     }
 
-        const htmlAttributesReference = {
-            "socket-html-attributes": ['broadcast', 'broadcast-sender', 'broadcast-browser', 'namespace', 'room', 'balancer'],
-            "crud-html-attributes": ['storage', 'database', 'array', 'object', 'key', 'index', 'save', 'read', 'update', 'delete', 'realtime', 'crud', 'upsert', 'value-type', 'value-prefix', 'value-suffix', 'listen'],
-            "filter-html-attributes": ['filter-selector', 'filter-closest', 'filter-parent', 'filter-previous', 'filter-next', 'filter-key', 'filter-value', 'filter-value-type', 'filter-case-sensitive', 'filter-operator', 'filter-logical-opertor', 'filter-sort-key', 'filter-sort-direction', 'filter-search', 'filter-limit', 'filter-count', 'filter-on'],
-            "render-html-attributes": ['render', 'render-selector', 'render-closest', 'render-parent', 'render-previous', 'render-next', 'render-as']
-        }
+    //     const htmlAttributesReference = {
+    //         "socket-html-attributes": ['broadcast', 'broadcast-sender', 'broadcast-browser', 'namespace', 'room', 'balancer'],
+    //         "crud-html-attributes": ['storage', 'database', 'array', 'object', 'key', 'index', 'save', 'read', 'update', 'delete', 'realtime', 'crud', 'upsert', 'value-type', 'value-prefix', 'value-suffix', 'listen'],
+    //         "filter-html-attributes": ['filter-selector', 'filter-closest', 'filter-parent', 'filter-previous', 'filter-next', 'filter-key', 'filter-value', 'filter-value-type', 'filter-case-sensitive', 'filter-operator', 'filter-logical-opertor', 'filter-sort-key', 'filter-sort-direction', 'filter-search', 'filter-limit', 'filter-count', 'filter-on'],
+    //         "render-html-attributes": ['render', 'render-selector', 'render-closest', 'render-parent', 'render-previous', 'render-next', 'render-as']
+    //     }
 
-        conversation = [
-            { role: 'system', content: 'If the users request seem to want to perform a CRUD operation, return a CoCreateJS CRUD data object as a response. Else reply as best you can to users queries' },
-            { role: 'system', content: 'data.method should default to "object.create", "object.read", "object.update", "object.delete"' },
-            { role: 'system', content: 'To perform CRUD operations on the objects contained within an array, use the following methods: "object.create" for creating objects, "object.read" for reading and returning one or more objects, "object.update" for updating objects, and "object.delete" for deleting objects. The array property must be defined to perform crud operations on objects' },
-            { role: 'system', content: 'data.storage and data.database is not required and should only be defined if the user specifically requests it. example: delete test database from indexeddb storage' },
-            { role: 'system', content: 'In the context of CoCreateJS, an "array" corresponds to a "table" in SQL databases or a "collection" in NoSQL databases.' },
-            { role: 'system', content: 'In the context of CoCreateJS, an "object" corresponds to a "row" in SQL databases or a "document" in NoSQL databases.' },
-            { role: 'system', content: 'crud reference' + JSON.stringify(crudReference) },
-            { role: 'system', content: 'crud object reference' + JSON.stringify(crudObjectReference) },
-            { role: 'system', content: 'When using object.update or object.delete methods the data.object._id should be defined or a filter used to return and excute on matches' },
-            { role: 'system', content: 'file object reference' + JSON.stringify(filterObjectReference) },
-            { role: 'system', content: 'If the users request seem to want to create a file or code return the code/source in the data.object.src . This will make the file available over network request using the defined path' },
-            { role: 'system', content: 'html attributes reference' + JSON.stringify(htmlAttributesReference) },
-            { role: 'system', content: 'component reference' + JSON.stringify(componentsReference) },
+    //     conversation = [
+    //         { role: 'system', content: 'If the users request seem to want to perform a CRUD operation, return a CoCreateJS CRUD data object as a response. Else reply as best you can to users queries' },
+    //         { role: 'system', content: 'data.method should default to "object.create", "object.read", "object.update", "object.delete"' },
+    //         { role: 'system', content: 'To perform CRUD operations on the objects contained within an array, use the following methods: "object.create" for creating objects, "object.read" for reading and returning one or more objects, "object.update" for updating objects, and "object.delete" for deleting objects. The array property must be defined to perform crud operations on objects' },
+    //         { role: 'system', content: 'data.storage and data.database is not required and should only be defined if the user specifically requests it. example: delete test database from indexeddb storage' },
+    //         { role: 'system', content: 'In the context of CoCreateJS, an "array" corresponds to a "table" in SQL databases or a "collection" in NoSQL databases.' },
+    //         { role: 'system', content: 'In the context of CoCreateJS, an "object" corresponds to a "row" in SQL databases or a "document" in NoSQL databases.' },
+    //         { role: 'system', content: 'crud reference' + JSON.stringify(crudReference) },
+    //         { role: 'system', content: 'crud object reference' + JSON.stringify(crudObjectReference) },
+    //         { role: 'system', content: 'When using object.update or object.delete methods the data.object._id should be defined or a filter used to return and excute on matches' },
+    //         { role: 'system', content: 'file object reference' + JSON.stringify(filterObjectReference) },
+    //         { role: 'system', content: 'If the users request seem to want to create a file or code return the code/source in the data.object.src . This will make the file available over network request using the defined path' },
+    //         { role: 'system', content: 'html attributes reference' + JSON.stringify(htmlAttributesReference) },
+    //         { role: 'system', content: 'component reference' + JSON.stringify(componentsReference) },
 
-        ]
-
-        forms.set(form, conversation)
-    }
+    //     ]
+    if (!conversation)
+        forms.set(form, conversation = [])
+    // }
 
     // 3 types avialable system, user, assistant
     for (let element of elements) {
         let role = element.getAttribute('openai')
-        let content
-        if (element.hasAttribute('crud')) {
-            // let json = JSON.parse(element.getAttribute('crud'))
-            // content = await crud.send()
-        } else {
-            content = await element.getValue()
+        if (!['system', 'user', 'assistant'].includes(role))
+            continue
+        let content = await element.getValue()
+        if (typeof content === 'string')
+            conversation.push({ role, content })
+        // if (Array.isArray(content))
+        //     conversation.push({ role, content })
+        if (typeof content === 'object') {
+            if (content.method) {
+                let type = content.method.split('.')[0]
+                for (let i = 0; i < content[type].length; i++) {
+                    if (typeof content[type][i].content === 'string')
+                        conversation.push({ role, content: content[type][i].content })
+                    else
+                        conversation.push({ role, content: JSON.stringify(content[type][i]) })
+                }
+            } else
+                conversation.push({ role, content: JSON.stringify(content) })
         }
+
+        // if (element.hasAttribute('crud')) {
+        //     // let json = JSON.parse(element.getAttribute('crud'))
+        //     // content = await crud.send()
+        // } else {
+        //     content = await element.getValue()
+        // }
 
         // if (role === 'system' && !systemMessages.has(content)) {
         // systemMessages.set(content, true)
 
-        if (role === 'system') {
-            conversation.push({ role, content })
+        // if (role === 'system') {
+        //     conversation.push({ role, content })
 
-            // if (content === 'componentsReference')
-            //     conversation.push({ role, content: JSON.stringify(componentsReference) })
-            // else
-            //     conversation.push({ role, content })
-        } else if (role === 'user' && content) {
-            conversation.push({ role, content })
-        } else if (role === 'message' && content)
-            conversation.push({ role: 'user', content })
+        //     // if (content === 'componentsReference')
+        //     //     conversation.push({ role, content: JSON.stringify(componentsReference) })
+        //     // else
+        //     //     conversation.push({ role, content })
+        // } else if (role === 'user' && content) {
+        //     conversation.push({ role, content })
+        // } else if (role === 'message' && content)
+        //     conversation.push({ role: 'user', content })
     }
 
     if (conversation.length)
@@ -197,10 +216,6 @@ async function sendMessage(messages) {
             }),
         };
 
-        document.dispatchEvent(new CustomEvent('openAi', {
-            detail: {}
-        }));
-
         const response = await fetch(apiUrl, requestOptions);
         const data = await response.json();
 
@@ -221,6 +236,10 @@ async function sendMessage(messages) {
         //         console.error('Invalid CoCreateJS API function:', component, action);
         //     }
         // }
+        document.dispatchEvent(new CustomEvent('openAi', {
+            detail: {}
+        }));
+
     } catch (error) {
         console.error('Error:', error);
     }
